710c2f334c1ca2b7674f73580c193c70e28e4d85
Emma <calyn2008@126.com>
2018-10-23 15:02:57 +0800

    init

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Emma on 2018/10/23.
+//  Copyright © 2018 Emma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
